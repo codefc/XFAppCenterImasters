715dc58dfa6f7c94efbe9f4f05ddea751880743d
Code FC <codefcbrasil@gmail.com>
2017-12-30 01:12:20 -0200

    Primeira versão do código fonte

---
+++ src/XFAppCenterImasters/XFAppCenterImasters/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XFAppCenterImasters.ViewModel.Base
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T property, T value, [CallerMemberName]string propertyName = "",  Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(property, value))
+                return false;
+
+            property = value;
+            onChanged?.Invoke();
+            RaiseProperty(propertyName);
+            return true;
+        }
+
+        protected void RaiseProperty([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ src/XFAppCenterImasters/XFAppCenterImasters/ViewModel/StartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XFAppCenterImasters.ViewModel.Base;
+
+namespace XFAppCenterImasters.ViewModel
+{
+    public class StartViewModel : BaseViewModel
+    {
+    }
+}
